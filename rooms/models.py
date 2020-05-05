ed541f96af8981ae7f6becbf5755b4526636e71d
JUNWON-OH <60842561+JUNWON-OH@users.noreply.github.com>
2020-05-05 23:39:45 +0900

    dt

--- rooms/models.py
+++ rooms/models.py
@@ -23,4 +23,3 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    whatsup = models.IntegerField()
