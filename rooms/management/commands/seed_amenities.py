c7ec3f0afc92d0a64150b7e00ddc8482d644b412
JUNWON-OH <dhwnsdnjs96@gmail.com>
2020-05-28 14:49:56 +0900

    make Command & Seed

---
+++ rooms/management/commands/seed_amenities.py
@@ -0,0 +1,34 @@
+from django.core.management.base import BaseCommand
+from rooms.models import Amenity
+
+
+class Command(BaseCommand):
+
+    help = "This command tells me love me"
+
+    def handle(self, *args, **options):
+        amenities = [
+            "Kitchen",
+            "Shampoo",
+            "Heating",
+            "Air conditioning",
+            "Washer",
+            "Dryer",
+            "Wifi",
+            "Breakfast",
+            "Indoor fireplace",
+            "Hangers",
+            "Iron",
+            "Hair dryer",
+            "Laptop-friendly workspace",
+            "TV",
+            "Crib",
+            "High chair",
+            "Self check-in",
+            "Smoke alarm",
+            "Carbon monoxide alarm",
+            "Private bathroom",
+        ]
+        for a in amenities:
+            Amenity.objects.create(name=a)
+        self.stdout.write(self.style.SUCCESS("Amenities created!"))
